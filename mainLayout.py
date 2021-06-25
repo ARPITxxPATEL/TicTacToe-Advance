01bdc86048620b7ea70d78f3080ebe43915baf0e
Arpit Patel <arpiitkumar2105@gmail.com>
2021-06-25 23:32:40 +0530

    Implement AI

--- mainLayout.py
+++ mainLayout.py
@@ -4,6 +4,7 @@ import cv2
 from PIL import Image, ImageTk, ImageColor
 import HandTrackingModule as htm
 import math
+import random
 
 root = Tk()
 root.title("Tic Tac Toe")
@@ -23,6 +24,14 @@ boardCenters = [
     (w // 2, 5 * h // 6),
     (5 * w // 6, 5 * h // 6),
 ]
+boardPoints = [
+    (w // 3, h // 3),
+    (2 * w / 3, h // 3),
+    (w // 3, 2 * h // 3),
+    (2 * w // 3, 2 * h // 3),
+]
+isGameOver = None
+gameScore = {"x": -1, "o": 1, "tie": 0}
 
 
 def choose_color(currbtn):
@@ -31,8 +40,10 @@ def choose_color(currbtn):
 
 
 def resetBoard():
-    global board
+    global board, isGameOver
     board = [""] * 9
+    isGameOver = ""
+    resultLabel["text"] = ""
 
 
 def convertHexToBGR(color):
@@ -61,9 +72,124 @@ def drawBoardOnFrame(img, colorInHex):
     cv2.line(img, (10, int(2 * h / 3)), (w - 10, int(2 * h / 3)), BGRcolor, 4)
 
 
+def findCellOnBoard(x, y):
+    if (x <= boardPoints[0][0]) and (y <= boardPoints[0][1]):
+        return 0
+    if (x >= boardPoints[0][0] and x <= boardPoints[1][0]) and (y <= boardPoints[0][1]):
+        return 1
+    if (x >= boardPoints[1][0]) and (y <= boardPoints[1][1]):
+        return 2
+    if (x <= boardPoints[0][0]) and (y >= boardPoints[0][1] and y <= boardPoints[2][1]):
+        return 3
+    if (x >= boardPoints[0][0] and x <= boardPoints[1][0]) and (
+        y >= boardPoints[0][1] and y <= boardPoints[2][1]
+    ):
+        return 4
+    if (x >= boardPoints[1][0]) and (y >= boardPoints[0][1] and y <= boardPoints[2][1]):
+        return 5
+    if (x <= boardPoints[0][0]) and (y >= boardPoints[2][1]):
+        return 6
+    if (x >= boardPoints[0][0] and x <= boardPoints[1][0]) and (y >= boardPoints[2][1]):
+        return 7
+    if (x >= boardPoints[1][0]) and (y >= boardPoints[2][1]):
+        return 8
+
+
+def checkForEndGame(capframe, draw=True):
+    point1, point2, sign, color = None, None, None, None
+    if board[0] == board[1] == board[2] != "":
+        point1, point2, sign = 0, 2, board[0]
+    if board[3] == board[4] == board[5] != "":
+        point1, point2, sign = 3, 5, board[3]
+    if board[6] == board[7] == board[8] != "":
+        point1, point2, sign = 6, 8, board[6]
+
+    if board[0] == board[3] == board[6] != "":
+        point1, point2, sign = 0, 6, board[0]
+    if board[1] == board[4] == board[7] != "":
+        point1, point2, sign = 1, 7, board[1]
+    if board[2] == board[5] == board[8] != "":
+        point1, point2, sign = 2, 8, board[2]
+
+    if board[0] == board[4] == board[8] != "":
+        point1, point2, sign = 0, 8, board[0]
+    if board[2] == board[4] == board[6] != "":
+        point1, point2, sign = 2, 6, board[2]
+
+    emptyCellCount = 0
+    for i in range(9):
+        if board[i] == "":
+            emptyCellCount += 1
+    if emptyCellCount == 0:
+        sign = "tie"
+
+    if sign == None:
+        return None
+
+    if draw:
+        if sign == "x":
+            color = XColorButton["bg"]
+            resultLabel["text"] = "You Lose!"
+            resultLabel["fg"] = "red"
+        elif sign == "o":
+            color = OColorButton["bg"]
+            resultLabel["text"] = "You Win!"
+            resultLabel["fg"] = "green"
+        else:
+            resultLabel["text"] = "Tie!"
+            resultLabel["fg"] = "black"
+
+        BGRcolor = convertHexToBGR(color)
+        cv2.line(capframe, boardCenters[point1], boardCenters[point2], BGRcolor, 4)
+    return sign
+
+
+def minimax(capframe, depth, isMaximizing):
+    global board
+    result = checkForEndGame(capframe, draw=False)
+    if result != None:
+        return gameScore[result]
+    if depth == 3:
+        return 0
+
+    if isMaximizing:
+        bestScore = float("-inf")
+        for i in range(9):
+            if board[i] == "":
+                board[i] = ai
+                score = minimax(capframe, depth + 1, False)
+                board[i] = ""
+                bestScore = max(score, bestScore)
+        return bestScore
+    else:
+        bestScore = float("inf")
+        for i in range(9):
+            if board[i] == "":
+                board[i] = player
+                score = minimax(capframe, depth + 1, True)
+                board[i] = ""
+                bestScore = min(score, bestScore)
+        return bestScore
+
+
+def bestMovePossible(capframe):
+    global board
+    bestScore = float("-inf")
+    bestMove = 0
+    for i in range(9):
+        if board[i] == "":
+            board[i] = ai
+            score = minimax(capframe, 0, False)
+            board[i] = ""
+            if score > bestScore:
+                bestScore = score
+                bestMove = i
+    return bestMove
+
+
 # Camera Frame
 camFrame = LabelFrame(root, text="Camera", width=500, height=400)
-camFrame.grid(row=0, column=0, padx=10, pady=5, columnspan=2)
+camFrame.grid(row=0, column=0, padx=10, pady=5, columnspan=3)
 
 # Label for camera
 camFrameLabel = Label(camFrame)
@@ -116,26 +242,23 @@ OColorButton.grid(row=0, column=3, padx=5, pady=5)
 resetButton = Button(root, text="Reset", width=10, command=lambda: resetBoard())
 resetButton.grid(row=1, column=1)
 
+# Result
+resultLabel = Label(root, text="", font=10)
+resultLabel.grid(row=1, column=2)
+
 
 cap = cv2.VideoCapture(0)
-detector = htm.handDetector(detectionCon=0.3)
+detector = htm.handDetector(detectionCon=0.75)
 
 
 def showFrames():
+    global isGameOver
     sucess99, capframe = cap.read()
     capframe = cv2.resize(capframe, (w, h))
     capframe = cv2.flip(capframe, 1)
 
-    drawBoardOnFrame(capframe, boardColorButton["bg"])
-
-    for i in range(9):
-        if board[i] == "x":
-            drawSignOnFrame(capframe, boardCenters[i], "x", XColorButton["bg"])
-        if board[i] == "o":
-            drawSignOnFrame(capframe, boardCenters[i], "o", OColorButton["bg"])
-
-    capframe = detector.findHands(capframe, draw=True)
-    lmList = detector.findPosition(capframe, draw=True)
+    capframe = detector.findHands(capframe, draw=False)
+    lmList = detector.findPosition(capframe, draw=False)
 
     if len(lmList) != 0:
         x1, y1 = lmList[4][1], lmList[4][2]
@@ -147,6 +270,27 @@ def showFrames():
         cv2.line(capframe, (x1, y1), (x2, y2), (255, 0, 255), 2)
 
         length = math.hypot(x2 - x1, y2 - y1)
+
+        if length < 50 and isGameOver == None:
+            cv2.circle(
+                capframe, (cx, cy), 3, convertHexToBGR(XColorButton["bg"]), cv2.FILLED
+            )
+            playerCellIndex = findCellOnBoard(cx, cy)
+            if board[playerCellIndex] == "":
+                board[playerCellIndex] = player
+                isGameOver = checkForEndGame(capframe)
+                if isGameOver == None:
+                    aiMove = bestMovePossible(capframe)
+                    board[aiMove] = ai
+
+    isGameOver = checkForEndGame(capframe)
+
+    drawBoardOnFrame(capframe, boardColorButton["bg"])
+    for i in range(9):
+        if board[i] == "x":
+            drawSignOnFrame(capframe, boardCenters[i], "x", XColorButton["bg"])
+        if board[i] == "o":
+            drawSignOnFrame(capframe, boardCenters[i], "o", OColorButton["bg"])
 
     cv2image = cv2.cvtColor(capframe, cv2.COLOR_BGR2RGB)
     img = Image.fromarray(cv2image)
